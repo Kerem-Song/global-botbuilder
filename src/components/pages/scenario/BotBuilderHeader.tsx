b8c07a497439f8e662a483aa48264833f396cdad
Kerem <pn200609@PN200609ui-MacBookPro.local>
2022-11-18 19:01:39 +0900

    feat: botbuilder header (making buttons)

--- src/components/pages/scenario/BotBuilderHeader.tsx
+++ src/components/pages/scenario/BotBuilderHeader.tsx
@@ -1,3 +1,57 @@
+import { Button } from '@components/general/Button';
+
 export const BotBuilderHeader = () => {
-  return <div>Bot builder Header</div>;
+  const cardNum = 12;
+  return (
+    <div className="botBuilderHeader">
+      <div className="botBuilderMakerWrapper">
+        <span className="cardNumWrapper">
+          말풍선 <span>{cardNum}</span>
+        </span>
+        <div className="makingBtn">
+          <span>단일</span>
+          <Button>
+            <i className="fa-solid fa-t"></i>
+          </Button>
+          <Button>
+            <i className="fa-regular fa-image"></i>
+          </Button>
+
+          <Button>
+            <i className="fa-solid fa-images"></i>
+          </Button>
+          <Button>
+            <i className="fa-regular fa-rectangle-list"></i>
+          </Button>
+          <Button>
+            <i className="fa-brands fa-shopify"></i>
+          </Button>
+        </div>
+        <div className="makingBtn">
+          <span>캐로셀</span>
+          <Button>
+            <i className="fa-solid fa-images"></i>
+          </Button>
+          <Button>
+            <i className="fa-solid fa-rectangle-list"></i>
+          </Button>
+          <Button>
+            <i className="fa-solid fa-bag-shopping"></i>
+          </Button>
+        </div>
+        <div className="makingBtn">
+          <span>버튼</span>
+          <Button>
+            <i className="fa-solid fa-toggle-on"></i>
+          </Button>
+          <Button>
+            <i className="fa-solid fa-check"></i>
+          </Button>
+        </div>
+      </div>
+      <div className="saveBtn">
+        <Button>저장하기</Button>
+      </div>
+    </div>
+  );
 };
