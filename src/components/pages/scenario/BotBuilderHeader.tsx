ce787f5807ea951e31b077ad24ea68cb09b93762
N210203 <bykim@lunasoft.co.kr>
2022-12-02 16:06:27 +0900

    fix: Bot Builder Header

--- src/components/pages/scenario/BotBuilderHeader.tsx
+++ src/components/pages/scenario/BotBuilderHeader.tsx
@@ -1,3 +1,15 @@
+import {
+  icBtnTemple,
+  icCaroCommerce,
+  icCaroImg,
+  icCaroList,
+  icCommerce,
+  icCondition,
+  icImg,
+  icList,
+  icQuickBtn,
+  icText,
+} from '@assets';
 import { Button } from '@components/general/Button';
 
 export const BotBuilderHeader = () => {
@@ -6,51 +18,55 @@ export const BotBuilderHeader = () => {
     <div className="botBuilderHeader">
       <div className="botBuilderMakerWrapper">
         <span className="cardNumWrapper">
-          말풍선 <span>{cardNum}</span>
+          chat bubble <span className="cardNum">{cardNum}</span>
         </span>
         <div className="makingBtn">
-          <span className="btnCategory">단일</span>
-          <Button>
-            <i className="fa-solid fa-t"></i>
+          <span className="btnCategory">Single</span>
+          <Button className="btnWrapper">
+            <img src={icText} alt="icText"></img>
           </Button>
-          <Button>
-            <i className="fa-regular fa-image"></i>
+          <Button className="btnWrapper">
+            <img src={icImg} alt="icImg"></img>
           </Button>
 
-          <Button>
-            <i className="fa-solid fa-images"></i>
+          <Button className="btnWrapper">
+            <img src={icBtnTemple} alt="icBtnTemple"></img>
           </Button>
-          <Button>
-            <i className="fa-regular fa-rectangle-list"></i>
+          <Button className="btnWrapper">
+            <img src={icList} alt="icList"></img>
           </Button>
-          <Button>
-            <i className="fa-brands fa-shopify"></i>
-          </Button>
-        </div>
-        <div className="makingBtn">
-          <span className="btnCategory">캐로셀</span>
-          <Button>
-            <i className="fa-solid fa-images"></i>
-          </Button>
-          <Button>
-            <i className="fa-solid fa-rectangle-list"></i>
-          </Button>
-          <Button>
-            <i className="fa-solid fa-bag-shopping"></i>
+          <Button className="btnWrapper">
+            <img src={icCommerce} alt="icCommerce"></img>
           </Button>
         </div>
+
         <div className="makingBtn">
-          <span className="btnCategory">버튼</span>
-          <Button>
-            <i className="fa-solid fa-toggle-on"></i>
+          <span className="btnCategory">Carousel</span>
+          <Button className="btnWrapper">
+            <img src={icCaroImg} alt="icCaroImg"></img>
           </Button>
-          <Button>
-            <i className="fa-solid fa-check"></i>
+          <Button className="btnWrapper">
+            <img src={icCaroList} alt="icCaroList"></img>
+          </Button>
+          <Button className="btnWrapper">
+            <img src={icCaroCommerce} alt="icCaroCommerce"></img>
+          </Button>
+        </div>
+
+        <div className="makingBtn">
+          <span className="btnCategory">Button</span>
+          <Button className="btnWrapper">
+            <img src={icQuickBtn} alt="icQuickBtn"></img>
+          </Button>
+          <Button className="btnWrapper">
+            <img src={icCondition} alt="icCondition"></img>
           </Button>
         </div>
       </div>
       <div className="saveBtn">
-        <Button>저장하기</Button>
+        <Button small type="primary">
+          Save
+        </Button>
       </div>
     </div>
   );
