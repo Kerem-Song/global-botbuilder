0702b21b9f1a8ad8d229f965e71711e0253c66bc
cain <yhhang1@lunasoft.co.kr>
2022-12-28 11:37:51 +0900

    feat: bot builder grid, scenario api

--- src/hooks/client/scenarioClient.ts
+++ src/hooks/client/scenarioClient.ts
@@ -1,14 +1,17 @@
 import { IScenarioModel } from '@models';
 import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router';
 
 import { useHttp } from '../useHttp';
 
 export const useScenarioClient = () => {
   const http = useHttp();
-
+  const { scenarioId } = useParams();
   const getScenarioList = useQuery<IScenarioModel[]>(['scenario-list'], () =>
     http
-      .get('https://636c4e007f47ef51e145ed03.mockapi.io/api/scenario')
+      .get(
+        `https://634d41c1f5d2cc648ea0bf80.mockapi.io/bot-list/${scenarioId}/scenario-list`,
+      )
       .then((res) => res.data),
   );
 
