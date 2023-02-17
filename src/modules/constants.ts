24d61d9cf7e8644e4202673c685264529ccdd4df
cain <yhhang1@lunasoft.co.kr>
2023-02-17 17:42:39 +0900

    fix: 드래그 쪼금만했을때 노드선택으로

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -3,3 +3,4 @@ export const NEXT_BUTTON_PREFIX = 'next-';
 export const NODE_PREFIX = 'node-';
 export const TRUE_SUFFIX = '-true';
 export const FALSE_SUFFIX = '-false';
+export const NODE_DRAG_FACTOR = 10;
