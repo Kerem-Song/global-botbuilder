e9b530f207313021f1095dcfc7d363ba32e5a95d
N210203 <bykim@lunasoft.co.kr>
2023-05-10 12:11:51 +0900

    fix: divide UtteranceDetail component

--- src/components/pages/utterance/UtteranceDetail.tsx
+++ src/components/pages/utterance/UtteranceDetail.tsx
@@ -1,55 +1,34 @@
-import { icEnter, icNoResult } from '@assets';
-import { Card } from '@components/data-display';
-import { Checkbox, FormItem, Input } from '@components/data-entry';
-import { Button } from '@components/general';
-import { Col, Row, Space } from '@components/layout';
+import { Button } from '@components';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useI18n, usePage, useRootState, useSystemModal } from '@hooks';
-import { useScenarioSelectClient } from '@hooks/client/scenarioSelectClient';
+import { usePage, useRootState, useSystemModal } from '@hooks';
 import { useUtteranceClient } from '@hooks/client/utteranceClient';
 import { usePrompt } from '@hooks/usePrompt';
-import {
-  IDeleteIntent,
-  IReactSelect,
-  ISaveIntent,
-  IUtteranceItem,
-  IUtteranceModel,
-} from '@models';
+import { IDeleteIntent, ISaveIntent, IUtteranceItem, IUtteranceModel } from '@models';
 import { BOTNAME_REGEX } from '@modules';
-import { util } from '@modules/util';
 import { useEffect, useRef, useState } from 'react';
-import { useController, useFieldArray, useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
-import Select from 'react-select';
 import * as yup from 'yup';
 
 import { lunaToast } from '../../../../src/modules/lunaToast';
-import { reactSelectStyle } from '../scenario/edit/ButtonCtrlSelector';
+import { AddUtterance } from './AddUtterance';
+import { UtteranceDetailItems } from './UtteranceDetailItems';
+import { UtteranceGroupInfo } from './UtteranceGroupInfo';
 
 export const UtteranceDetail = () => {
-  const { i18n } = useI18n();
+  const [isActive, setIsActive] = useState<boolean>(false);
   const { navigate, t, tc } = usePage();
   const { utteranceId, botId } = useParams();
-  const { confirm, error } = useSystemModal();
-  const { getScenarioList } = useScenarioSelectClient();
-  const { data } = getScenarioList();
   const {
     intentAsync,
     getIntentDetailQuery,
     intentDeleteAsync,
     checkIntentDuplicationAsync,
-    checkUtteranceDuplicationAsync,
   } = useUtteranceClient();
   const hasUtteranceId = getIntentDetailQuery(utteranceId);
-  const language = i18n.language;
   const token = useRootState((state) => state.botInfoReducer.token);
-  const utteranceRef = useRef<HTMLInputElement>(null);
   const intentNameRef = useRef<HTMLInputElement | null>(null);
-  const [totalScenarioList, setTotalScenarioList] = useState<IReactSelect[]>();
-  const [searchWord, setSearchWord] = useState<string>('');
-  const [utteranceWord, setUtteranceWord] = useState<string>('');
-  const [isActive, setIsActive] = useState<boolean>(false);
-  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const { confirm, error } = useSystemModal();
 
   // usePrompt(isActive);
 
@@ -63,64 +42,26 @@ export const UtteranceDetail = () => {
       }),
   });
 
-  const {
-    reset,
-    register,
-    handleSubmit,
-    control,
-    getValues,
-    watch,
-    formState: { errors },
-  } = useForm<IUtteranceModel>({
-    defaultValues: {
-      name: '',
-      connectScenarioId: '',
-      items: [],
-    },
+  const defaultValues = {
+    name: '',
+    connectScenarioId: '',
+    items: [],
+  };
+
+  const formMethods = useForm<IUtteranceModel>({
+    defaultValues,
     resolver: yupResolver(schema),
   });
 
-  const { field: nameField } = useController({
-    name: `name`,
-    control,
-  });
-
-  const { field: scenarioListField } = useController({
-    name: `connectScenarioId`,
-    control,
-  });
+  const { reset, handleSubmit, control, getValues, watch } = formMethods;
 
   const { fields, remove, prepend } = useFieldArray({ control, name: 'items' });
 
-  const filterKeyword = fields.filter((x) =>
-    x.text?.trim().toLowerCase().includes(searchWord.trim().toLowerCase()),
-  );
-
   const handleListBtn = async () => {
-    if (isActive === true) {
+    if (isActive) {
       navigate(`/${botId}/utterance`);
     } else {
       navigate(`/${botId}/utterance`);
-    }
-  };
-
-  const openDeleteCheckboxModal = async () => {
-    const deleteItems = getValues().items.filter((x) => x.isChecked);
-
-    if (deleteItems.length === 0) {
-      return;
-    }
-    const result = await confirm({
-      title: t('DELETE_UTTERANCE'),
-      description: <span>{t('DELETE_CONFIRM', { count: deleteItems.length })}</span>,
-    });
-
-    if (result) {
-      deleteItems.map((item) => {
-        setIsActive(true);
-        const index = getValues().items.indexOf(item);
-        remove(index);
-      });
     }
   };
 
@@ -147,66 +88,6 @@ export const UtteranceDetail = () => {
         }
       }
     }
-  };
-
-  const handleAddUtternace = () => {
-    if (!utteranceWord || !utteranceWord.trim()) return;
-
-    if (
-      getValues('items')
-        .map((x) => x.text)
-        ?.includes(utteranceWord)
-    ) {
-      error({
-        title: t('DUPLICATE_UTTERANCE'),
-        description: (
-          <div style={{ whiteSpace: 'pre-wrap' }}>
-            <span>{t('DUPLICATE_UTTERANCE_MESSAGE')}</span>
-            <span style={{ color: 'red' }}>{getValues('name')}</span>
-          </div>
-        ),
-      });
-      if (utteranceRef.current) {
-        utteranceRef.current.select();
-      }
-      return;
-    }
-
-    checkUtteranceDuplicationAsync(
-      {
-        text: utteranceWord,
-      },
-      {
-        onSuccess: (result) => {
-          console.log('result', result);
-          if (result.length > 0) {
-            error({
-              title: t('DUPLICATE_UTTERANCE'),
-              description: (
-                <div style={{ whiteSpace: 'pre-wrap' }}>
-                  <span>{t('DUPLICATE_UTTERANCE_MESSAGE')}</span>
-                  <span style={{ color: 'red' }}>{result[0].intentName}</span>
-                </div>
-              ),
-            });
-            if (utteranceRef.current) {
-              utteranceRef.current.select();
-            }
-          } else {
-            prepend({ text: utteranceWord });
-            setIsActive(true);
-            setUtteranceWord('');
-            if (utteranceRef.current) {
-              utteranceRef.current.focus();
-            }
-          }
-        },
-      },
-    );
-  };
-
-  const handleSearch = (keyword?: string) => {
-    setSearchWord(keyword!);
   };
 
   const handleSave = async (itemData: IUtteranceModel): Promise<void> => {
@@ -267,26 +148,7 @@ export const UtteranceDetail = () => {
     }
   }, [hasUtteranceId?.data]);
 
-  useEffect(() => {
-    const scenarioList = data
-      ?.filter((item) => !item.isFallbackFlow)
-      .map((x) => {
-        return { value: x.id, label: x.alias };
-      });
-
-    const total = [
-      { value: '', label: t('SELECT_SCENARIO') },
-      ...(scenarioList ? scenarioList : []),
-    ];
-
-    setTotalScenarioList(total);
-  }, [data, language]);
-
-  useEffect(() => {
-    if (nameField.value === '' && intentNameRef.current) {
-      intentNameRef.current.focus();
-    }
-  }, [nameField.value]);
+  console.log('isActive', isActive);
 
   return (
     <div className="utteranceDetailWrap">
@@ -313,164 +175,19 @@ export const UtteranceDetail = () => {
             </Button>
           </div>
         </div>
-        <Card
-          radius="normal"
-          bodyStyle={{ padding: '20px' }}
-          style={{ border: '1px solid #DCDCDC', marginTop: '20px' }}
-        >
-          <Space direction="vertical">
-            <p style={{ fontSize: '16px', fontWeight: 500 }}>{t('TITLE')}</p>
-            <Row align="center" gap={10}>
-              <Col style={{ width: '130px' }}>
-                <span>{t('INTENT_NAME')}</span>
-                <span style={{ color: 'red' }}> *</span>
-              </Col>
-              <Col flex="auto">
-                <FormItem error={errors.name}>
-                  <Input
-                    value={nameField.value}
-                    ref={(e) => {
-                      nameField.ref(e);
-                      intentNameRef.current = e;
-                    }}
-                    onChange={(e) => {
-                      nameField.onChange(e);
-                      setIsActive(true);
-                    }}
-                    placeholder={t('INPUT_INTENT_NAME')}
-                    maxLength={20}
-                    showCount
-                  />
-                </FormItem>
-              </Col>
-            </Row>
-            <Row align="center" gap={10}>
-              <Col style={{ width: '130px' }}>
-                <span>{t('CONNECT_SCENARIOS')}</span>
-              </Col>
-              <Col flex="auto">
-                <Select
-                  {...scenarioListField}
-                  options={totalScenarioList}
-                  value={totalScenarioList?.find(
-                    (item) => item.value === scenarioListField.value,
-                  )}
-                  onChange={(options: any) => {
-                    scenarioListField.onChange(options.value);
-                    setIsActive(true);
-                  }}
-                  styles={reactSelectStyle}
-                  isSearchable={false}
-                  placeholder={t('SELECT_SCENARIO')}
-                />
-              </Col>
-            </Row>
-          </Space>
-        </Card>
+        <UtteranceGroupInfo formMethods={formMethods} setIsActive={setIsActive} />
       </form>
-      <div className="utterance add">
-        <Space direction="vertical">
-          <p style={{ fontSize: '16px', fontWeight: 500 }}>{t('ADD_UTTERANCE')}</p>
-          <Row>
-            <Col flex="auto">
-              <Input
-                ref={utteranceRef}
-                value={utteranceWord}
-                onChange={(e) => {
-                  setUtteranceWord(e.target.value);
-                  setIsEditing(true);
-                }}
-                onPressEnter={handleAddUtternace}
-                placeholder={t('ENTER_UTTERANCE')}
-              />
-            </Col>
-            <Col style={{ marginLeft: '8px' }}>
-              <Button
-                type="primary"
-                style={{
-                  width: '64px',
-                  height: '33px',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-                onClick={() => {
-                  handleAddUtternace();
-                }}
-                disabled={utteranceWord && isEditing ? false : true}
-              >
-                <img src={icEnter} alt="enter" />
-              </Button>
-            </Col>
-          </Row>
-        </Space>
-      </div>
-      <div className="utterance list">
-        <Space direction="horizontal">
-          <span className="title">
-            {t('UTTERANCE')}{' '}
-            <span className="utteranceLength">
-              {filterKeyword ? filterKeyword.length : watch('items').length}
-            </span>
-          </span>
-          <FormItem>
-            <Input
-              size="small"
-              search
-              placeholder={t('SEARCH_UTTERANCE_PLACEHOLDER')}
-              value={searchWord}
-              onSearch={(value) => handleSearch(value)}
-              onChange={(e) => handleSearch(e.target.value)}
-            />
-          </FormItem>
-          <Button
-            shape="ghost"
-            className="icDelete"
-            onClick={openDeleteCheckboxModal}
-            disabled={
-              getValues('items') &&
-              getValues().items.filter((x) => x.isChecked).length === 0
-                ? true
-                : false
-            }
-          />
-        </Space>
-        <Row style={{ marginTop: '12px' }}>
-          <>
-            {watch('items').length === 0 ? (
-              <Row style={{ width: '100%', marginTop: '12px' }}>
-                <Col className="emptyList utteranceItem">
-                  <div className="empty">
-                    <img src={icNoResult} alt="empty" />
-                    <span>{t('NO_REGISTERED_UTTERANCE')}</span>
-                  </div>
-                </Col>
-              </Row>
-            ) : filterKeyword.length > 0 ? (
-              filterKeyword.map((v, i) => {
-                return (
-                  <div key={v.id} className="utteranceItem">
-                    <Checkbox
-                      {...register(`items.${i}.isChecked`)}
-                      style={{ marginLeft: '20px' }}
-                    />
-                    <p className="item">{util.replaceKeywordMark(v.text!, searchWord)}</p>
-                  </div>
-                );
-              })
-            ) : (
-              <Row style={{ width: '100%', marginTop: '12px' }}>
-                <div className="emptyList utteranceItem">
-                  <div className="empty">
-                    <img src={icNoResult} alt="empty" />
-                    <span>{t('NO_SEARCH_UTTERANCE_RESULT_FOUND')}</span>
-                  </div>
-                </div>
-              </Row>
-            )}
-          </>
-        </Row>
-      </div>
+      <AddUtterance
+        formMethods={formMethods}
+        prepend={prepend}
+        setIsActive={setIsActive}
+      />
+      <UtteranceDetailItems
+        formMethods={formMethods}
+        fields={fields}
+        remove={remove}
+        setIsActive={setIsActive}
+      />
     </div>
   );
 };
---
+++ src/components/pages/utterance/UtteranceGroupInfo.tsx
@@ -0,0 +1,120 @@
+import { Card, Col, FormItem, Input, Row, Space } from '@components';
+import { useI18n, usePage } from '@hooks';
+import { useScenarioSelectClient } from '@hooks/client/scenarioSelectClient';
+import { IReactSelect, IUtteranceModel } from '@models';
+import { Dispatch, FC, SetStateAction, useEffect, useRef, useState } from 'react';
+import { useController, UseFormReturn } from 'react-hook-form';
+import Select from 'react-select';
+
+import { reactSelectStyle } from '../scenario/edit/ButtonCtrlSelector';
+
+export interface IUtteranceGroupInfoProps {
+  formMethods: UseFormReturn<IUtteranceModel>;
+  setIsActive: Dispatch<SetStateAction<boolean>>;
+}
+
+export const UtteranceGroupInfo: FC<IUtteranceGroupInfoProps> = ({
+  formMethods,
+  setIsActive,
+}) => {
+  const { i18n } = useI18n();
+  const { t } = usePage();
+  const { getScenarioList } = useScenarioSelectClient();
+  const { data } = getScenarioList();
+
+  const language = i18n.language;
+  const intentNameRef = useRef<HTMLInputElement | null>(null);
+  const [totalScenarioList, setTotalScenarioList] = useState<IReactSelect[]>();
+  const {
+    control,
+    formState: { errors },
+  } = formMethods;
+
+  const { field: scenarioListField } = useController({
+    name: `connectScenarioId`,
+    control,
+  });
+
+  const { field: nameField } = useController({
+    name: `name`,
+    control,
+  });
+
+  useEffect(() => {
+    const scenarioList = data
+      ?.filter((item) => !item.isFallbackFlow)
+      .map((x) => {
+        return { value: x.id, label: x.alias };
+      });
+
+    const total = [
+      { value: '', label: t('SELECT_SCENARIO') },
+      ...(scenarioList ? scenarioList : []),
+    ];
+
+    setTotalScenarioList(total);
+  }, [data, language]);
+
+  useEffect(() => {
+    if (nameField.value === '' && intentNameRef.current) {
+      intentNameRef.current.focus();
+    }
+  }, [nameField.value]);
+
+  return (
+    <Card
+      radius="normal"
+      bodyStyle={{ padding: '20px' }}
+      style={{ border: '1px solid #DCDCDC', marginTop: '20px' }}
+    >
+      <Space direction="vertical">
+        <p style={{ fontSize: '16px', fontWeight: 500 }}>{t('TITLE')}</p>
+        <Row align="center" gap={10}>
+          <Col style={{ width: '130px' }}>
+            <span>{t('INTENT_NAME')}</span>
+            <span style={{ color: 'red' }}> *</span>
+          </Col>
+          <Col flex="auto">
+            <FormItem error={errors.name}>
+              <Input
+                value={nameField.value}
+                ref={(e) => {
+                  nameField.ref(e);
+                  intentNameRef.current = e;
+                }}
+                onChange={(e) => {
+                  nameField.onChange(e);
+                  setIsActive(true);
+                }}
+                placeholder={t('INPUT_INTENT_NAME')}
+                maxLength={20}
+                showCount
+              />
+            </FormItem>
+          </Col>
+        </Row>
+        <Row align="center" gap={10}>
+          <Col style={{ width: '130px' }}>
+            <span>{t('CONNECT_SCENARIOS')}</span>
+          </Col>
+          <Col flex="auto">
+            <Select
+              {...scenarioListField}
+              options={totalScenarioList}
+              value={totalScenarioList?.find(
+                (item) => item.value === scenarioListField.value,
+              )}
+              onChange={(options: any) => {
+                scenarioListField.onChange(options.value);
+                setIsActive(true);
+              }}
+              styles={reactSelectStyle}
+              isSearchable={false}
+              placeholder={t('SELECT_SCENARIO')}
+            />
+          </Col>
+        </Row>
+      </Space>
+    </Card>
+  );
+};
