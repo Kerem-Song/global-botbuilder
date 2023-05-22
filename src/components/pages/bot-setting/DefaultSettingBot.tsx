daa655317fbe9a8b8a0e4e09c77ee2ea409ae892
N210203 <bykim@lunasoft.co.kr>
2023-05-22 11:51:49 +0900

    fix: setting validation

--- src/components/pages/bot-setting/DefaultSettingBot.tsx
+++ src/components/pages/bot-setting/DefaultSettingBot.tsx
@@ -1,21 +1,24 @@
 import { icCopy } from '@assets';
-import { Button, Card, Col, Input, Row, Space } from '@components';
+import { Button, Card, Col, FormItem, Input, Row, Space } from '@components';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useBotClient, usePage, useRootState, useSystemModal } from '@hooks';
+import { useBotClient, usePage, useRootState } from '@hooks';
+import { usePrompt } from '@hooks/usePrompt';
 import { IBotSetting } from '@models/interfaces/IBotSetting';
 import { BOTNAME_REGEX } from '@modules';
 import { lunaToast } from '@modules/lunaToast';
 import { util } from '@modules/util';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useController, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 export const DefaultSettingBot = () => {
   const { t, tc } = usePage();
-  const { confirm } = useSystemModal();
   const { botUpdateNameAsync } = useBotClient();
   const botInfo = useRootState((state) => state.botInfoReducer.botInfo);
+  const botNameRef = useRef<HTMLInputElement | null>(null);
   const [isSaveBtnActive, setIsSaveBtnActive] = useState<boolean>(false);
+
+  usePrompt(isSaveBtnActive);
 
   const botNameSchema = yup.object({
     botName: yup
@@ -28,48 +31,53 @@ export const DefaultSettingBot = () => {
       }),
   });
 
-  const {
-    reset,
-    trigger,
-    control,
-    formState: { errors },
-  } = useForm<IBotSetting>({
-    defaultValues: {
-      botId: botInfo?.id,
-      botName: botInfo?.botName,
-    },
+  const defaultValues = {
+    botId: '',
+    botName: '',
+  };
+
+  const formMethods = useForm<IBotSetting>({
+    defaultValues,
     resolver: yupResolver(botNameSchema),
   });
 
+  const {
+    reset,
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors },
+  } = formMethods;
+
   const { field } = useController({ name: 'botName', control });
+
+  useEffect(() => {
+    if (botInfo) {
+      reset({
+        botId: botInfo.id,
+        botName: botInfo.botName,
+      });
+    }
+  }, [botInfo]);
+
+  useEffect(() => {
+    if (field.value === '' && botNameRef.current) {
+      botNameRef.current.focus();
+    }
+  }, [field.value]);
 
   const handleBotName = (e: React.ChangeEvent<HTMLInputElement>) => {
     field.onChange(e);
     setIsSaveBtnActive(true);
   };
 
-  const handleUpdateBot = async () => {
-    const name = field.value.trim();
+  const handleSaveBotName = async () => {
+    const saveBotName = {
+      botId: getValues('botId'),
+      botName: getValues('botName').trim(),
+    };
 
-    if (!botInfo) {
-      return;
-    }
-
-    if (name === '') {
-      trigger('botName');
-      return;
-    } else if (name.length < 2) {
-      trigger('botName');
-      return;
-    } else if (!BOTNAME_REGEX.test(name)) {
-      trigger('botName');
-      return;
-    }
-
-    const res = await botUpdateNameAsync({
-      botId: botInfo.id,
-      botName: name !== botInfo.botName ? name : undefined,
-    });
+    const res = await botUpdateNameAsync(saveBotName);
 
     if (res?.data.isSuccess) {
       lunaToast.success(tc('SAVE_MESSAGE'));
@@ -82,86 +90,64 @@ export const DefaultSettingBot = () => {
     lunaToast.info(t('COPY_BOT_ID_MESSAGE'));
   };
 
-  const preventGoBack = async () => {
-    const result = await confirm({
-      title: t('SAVE'),
-      description: <p style={{ whiteSpace: 'pre-wrap' }}>{tc('SAVE_CONFIRM_MESSAGE')}</p>,
-    });
-    if (result) {
-      history.go(-1);
-    } else {
-      history.pushState(null, '', location.href);
-    }
-  };
-
-  useEffect(() => {
-    if (botInfo) {
-      reset({
-        botId: botInfo.id,
-        botName: botInfo.botName,
-      });
-    }
-  }, [botInfo]);
-
-  useEffect(() => {
-    if (isSaveBtnActive) {
-      (() => {
-        history.pushState(null, '', location.href);
-        window.addEventListener('popstate', preventGoBack);
-      })();
-
-      return () => {
-        window.removeEventListener('popstate', preventGoBack);
-      };
-    }
-  }, [isSaveBtnActive]);
-
   return (
     <Card className="settingCardWrap" radius="normal">
-      <form>
-        <Space direction="vertical">
-          <p className="settingCardTitle">{t('DEFAULT_SETTING')}</p>
+      <Space direction="vertical">
+        <p className="settingCardTitle">{t('DEFAULT_SETTING')}</p>
+        <form
+          role="presentation"
+          onSubmit={handleSubmit(handleSaveBotName)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+            }
+          }}
+        >
           <Row gap={10} align="center">
             <Col className="botInfo">
               <span>{t('BOT_NAME')}</span>
               <span className="required"> *</span>
             </Col>
             <Col flex="auto">
-              <Input
-                value={field.value || ''}
-                onChange={handleBotName}
-                isError={errors.botName?.message ? true : false}
-                placeholder={t('BOT_NAME_PLACEHOLDER')}
-                maxLength={20}
-                showCount
-              />
-              <span className="error-message">{errors.botName?.message}</span>
+              <FormItem error={errors.botName}>
+                <Input
+                  value={field.value || ''}
+                  ref={(e) => {
+                    field.ref(e);
+                    botNameRef.current = e;
+                  }}
+                  onChange={handleBotName}
+                  placeholder={t('BOT_NAME_PLACEHOLDER')}
+                  maxLength={20}
+                  showCount
+                />
+              </FormItem>
             </Col>
             <Col>
               <Button
                 type="primary"
+                htmlType="submit"
                 disabled={isSaveBtnActive ? false : true}
-                onClick={handleUpdateBot}
               >
                 {t('SAVE')}
               </Button>
             </Col>
           </Row>
-          <Row gap={10} align="center">
-            <Col className="botInfo">
-              <span>{t('BOT_ID')}</span>
-            </Col>
-            <Col flex="auto">
-              <Input className="botId" value={botInfo?.id || ''} disabled />
-            </Col>
-            <Col>
-              <Button onClick={handleCopyBotId} icon={icCopy}>
-                {t('COPY')}
-              </Button>
-            </Col>
-          </Row>
-        </Space>
-      </form>
+        </form>
+        <Row gap={10} align="center">
+          <Col className="botInfo">
+            <span>{t('BOT_ID')}</span>
+          </Col>
+          <Col flex="auto">
+            <Input className="botId" value={botInfo?.id || ''} disabled />
+          </Col>
+          <Col>
+            <Button onClick={handleCopyBotId} icon={icCopy}>
+              {t('COPY')}
+            </Button>
+          </Col>
+        </Row>
+      </Space>
     </Card>
   );
 };
