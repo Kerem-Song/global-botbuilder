93c57dfa32099e2ce34373c6841176f0c9037f17
N210203 <bykim@lunasoft.co.kr>
2023-05-30 16:29:25 +0900

    fix: bot tester hover

--- src/components/pages/scenario/bot-tester/TesterMessagesItem.tsx
+++ src/components/pages/scenario/bot-tester/TesterMessagesItem.tsx
@@ -118,57 +118,55 @@ export const TesterMessagesItem = ({ item, onClick }: TesterProps) => {
             role="presentation"
             onClick={() => onClick(item.debugMeta)}
           >
-            <div className="productCardWrap">
-              <div className="productCard">
-                <img
-                  className={
-                    item.image?.imageAspectRatio === 0
-                      ? 'productCardImg_rectangle'
-                      : 'productCardImg_square'
-                  }
-                  src={item.image?.imageUrl}
-                  alt="productCardCarouselImg"
-                />
-                <div className="productCardContents">
-                  <div className="productCardTitle">
-                    <div className="title">
-                      <img className="icon" src={item.icon.url} alt="iconImg" />
-                      <p className="name">{item.title}</p>
-                    </div>
+            <div className="productCard">
+              <img
+                className={
+                  item.image?.imageAspectRatio === 0
+                    ? 'productCardImg_rectangle'
+                    : 'productCardImg_square'
+                }
+                src={item.image?.imageUrl}
+                alt="productCardCarouselImg"
+              />
+              <div className="productCardContents">
+                <div className="productCardTitle">
+                  <div className="title">
+                    <img className="icon" src={item.icon.url} alt="iconImg" />
+                    <p className="name">{item.title}</p>
                   </div>
-                  <div className="productCardPrices">
-                    {item.price?.isShowDiscount && (
-                      <>
-                        <div className="price">
-                          <p className="prevPrice">{item.price.retailDisplay}</p>
-                        </div>
-                        <div className="discount">
-                          <p className="discountAmount">{item.price.discountDisplay}</p>
-                        </div>
-                      </>
-                    )}
-                    <div className="price">
-                      <p className="currentPrice">{item.price?.mainDisplay}</p>
-                    </div>
-                  </div>
-                  <div className="productContents">
-                    <div className="productDesc">
-                      <p className="desc">{item.description}</p>
-                    </div>
-                    {item.buttons.length > 0 ? (
-                      <div
-                        className={
-                          item.image?.imageAspectRatio === 0
-                            ? 'rectangleImageBtn'
-                            : 'squareImageBtn'
-                        }
-                      >
-                        {item.buttons?.map((v, i) => {
-                          return <TesterMessagesItemButton key={i} item={v} />;
-                        })}
+                </div>
+                <div className="productCardPrices">
+                  {item.price?.isShowDiscount && (
+                    <>
+                      <div className="price">
+                        <p className="prevPrice">{item.price.retailDisplay}</p>
                       </div>
-                    ) : null}
+                      <div className="discount">
+                        <p className="discountAmount">{item.price.discountDisplay}</p>
+                      </div>
+                    </>
+                  )}
+                  <div className="price">
+                    <p className="currentPrice">{item.price?.mainDisplay}</p>
                   </div>
+                </div>
+                <div className="productContents">
+                  <div className="productDesc">
+                    <p className="desc">{item.description}</p>
+                  </div>
+                  {item.buttons.length > 0 ? (
+                    <div
+                      className={
+                        item.image?.imageAspectRatio === 0
+                          ? 'rectangleImageBtn'
+                          : 'squareImageBtn'
+                      }
+                    >
+                      {item.buttons?.map((v, i) => {
+                        return <TesterMessagesItemButton key={i} item={v} />;
+                      })}
+                    </div>
+                  ) : null}
                 </div>
               </div>
             </div>
