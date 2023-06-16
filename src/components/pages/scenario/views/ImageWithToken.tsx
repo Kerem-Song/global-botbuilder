b318e9a6a1ff6bc0adc7497a9fc82a99f487e9be
yhsong@lunasoft.co.kr <keremcalki@gmail.com>
2023-06-16 14:45:18 +0900

    fix: img not found in node

--- src/components/pages/scenario/views/ImageWithToken.tsx
+++ src/components/pages/scenario/views/ImageWithToken.tsx
@@ -1,31 +1,35 @@
 import { icImgNotFound } from '@assets';
-import { useHttp, useRootState } from '@hooks';
-import { imageUploadClient } from '@hooks';
-import {
-  IBasicCardView,
-  IListCardView,
-  IProductCardView,
-} from '@models/interfaces/res/IGetFlowRes';
+import { usePage, useRootState } from '@hooks';
+import { createElement, useRef } from 'react';
 
 export const ImageWithToken = ({ origin }: { origin?: string }) => {
+  const { t } = usePage();
   const token = useRootState((state) => state.botInfoReducer.token);
 
   const builderImageSrc = `${
     import.meta.env.VITE_API_BASE_URL
   }/builderimage/forbuilder?origin=${origin}&sessionToken=${token}`;
 
+  const imgNotFoundTextRef = useRef<HTMLSpanElement>(null);
+
   return (
     <>
       {origin && builderImageSrc ? (
-        <img
-          src={builderImageSrc}
-          alt="thumbnailImage"
-          onError={(e) => {
-            e.currentTarget.onerror = null;
-            e.currentTarget.src = icImgNotFound;
-            e.currentTarget.className = 'imgNotFound';
-          }}
-        />
+        <>
+          <img
+            src={builderImageSrc}
+            alt="thumbnailImage"
+            onError={(e) => {
+              e.currentTarget.onerror = null;
+              e.currentTarget.src = icImgNotFound;
+              e.currentTarget.className = 'imgNotFound';
+              if (imgNotFoundTextRef.current) {
+                imgNotFoundTextRef.current.innerHTML = `${t(`IMAGE_NOT_FOUND`)}`;
+              }
+            }}
+          />
+          <span ref={imgNotFoundTextRef}></span>
+        </>
       ) : (
         <div className="skeleton"></div>
       )}
