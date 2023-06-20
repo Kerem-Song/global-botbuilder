ea34a5e848505b4c5944581dbd1119e3e2b9ba2c
N210203 <bykim@lunasoft.co.kr>
2023-06-20 12:24:38 +0900

    fix: bot tester cards

--- src/components/pages/scenario/bot-tester/ProductCardCarouselType.tsx
+++ src/components/pages/scenario/bot-tester/ProductCardCarouselType.tsx
@@ -15,87 +15,85 @@ export const ProductCardCarouselType: FC<IProductCardCarouselTypeProps> = ({
   handleImgOnError,
 }) => {
   return (
-    <div className="productCard">
-      <img
-        className={
-          item.image?.imageAspectRatio === 0
-            ? 'productCardImg_rectangle'
-            : 'productCardImg_square'
-        }
-        src={item.image?.imageUrl}
-        alt="productCardCarouselImg"
-        onError={(e) => {
-          handleImgOnError(e);
-        }}
-      />
-      <div className="productCardContents">
-        <div className="productCardTitle">
-          <div className="title">
-            <img
-              className="icon"
-              src={item.icon.url}
-              alt="iconImg"
-              onError={(e) => {
-                handleImgOnError(e);
-              }}
-            />
-            <MultiClamp clamp={1}>{item.title.substring(0, 39)}</MultiClamp>
+    <div
+      className={classNames('productCard', {
+        productCarouselRectangleImageCard: item.image?.imageAspectRatio === 0,
+        productCarouselSquareImageCard: item.image?.imageAspectRatio != 0,
+      })}
+    >
+      <div>
+        <img
+          className={
+            item.image?.imageAspectRatio === 0
+              ? 'productCardImg_rectangle'
+              : 'productCardImg_square'
+          }
+          src={item.image?.imageUrl}
+          alt="productCardCarouselImg"
+          onError={(e) => {
+            handleImgOnError(e);
+          }}
+        />
+      </div>
+      <div className="productCardContents productCardCarouselContents">
+        <div>
+          <div className="productCardTitle">
+            <div className="title">
+              <img
+                className="icon"
+                src={item.icon.url}
+                alt="iconImg"
+                onError={(e) => {
+                  handleImgOnError(e);
+                }}
+              />
+              <MultiClamp clamp={1}>{item.title.substring(0, 39)}</MultiClamp>
+            </div>
+          </div>
+          <div className="productCardCarouselWrap">
+            <div className="productCardPrices">
+              {item.price?.isShowDiscount && (
+                <>
+                  <div className="price">
+                    <p className="prevPrice">{item.price.retailDisplay}</p>
+                  </div>
+                  <div className="discount">
+                    <p className="discountAmount">{item.price.discountDisplay}</p>
+                  </div>
+                </>
+              )}
+              <div className="price">
+                <p className="currentPrice">{item.price?.mainDisplay}</p>
+              </div>
+            </div>
+            <div
+              className={classNames('productContents', {
+                productCarouselContents: item.image?.imageAspectRatio != 0,
+                productCarouselRectangleImageContents: item.image?.imageAspectRatio === 0,
+              })}
+            >
+              <div className="productDesc">
+                <MultiClamp clamp={2} ellipsis={'...'}>
+                  {item.description.substring(0, 59)}
+                </MultiClamp>
+              </div>
+            </div>
           </div>
         </div>
-        <div
-          className={classNames('productCardCarouselWrap', {
-            productCarouselRectangleImageWrap: item.image?.imageAspectRatio === 0,
-          })}
-        >
-          <div className="productCardPrices">
-            {item.price?.isShowDiscount && (
-              <>
-                <div className="price">
-                  <p className="prevPrice">{item.price.retailDisplay}</p>
-                </div>
-                <div className="discount">
-                  <p className="discountAmount">{item.price.discountDisplay}</p>
-                </div>
-              </>
-            )}
-            <div className="price">
-              <p className="currentPrice">{item.price?.mainDisplay}</p>
+        <div className="productCardCarouselBtnWrap">
+          {item.buttons.length > 0 ? (
+            <div
+              className={
+                item.image?.imageAspectRatio === 0
+                  ? 'rectangleImageBtn'
+                  : 'squareImageBtn'
+              }
+            >
+              {item.buttons?.map((v, i) => {
+                return <TesterMessagesItemButton key={i} item={v} />;
+              })}
             </div>
-          </div>
-          <div
-            className={classNames('productContents', {
-              productCarouselContents: item.image?.imageAspectRatio != 0,
-              productCarouselRectangleImageContents: item.image?.imageAspectRatio === 0,
-              productCarouselNoBtnContents: item.buttons.length <= 0,
-              productCarouselRectangleImageNoBtnContents:
-                item.image?.imageAspectRatio === 0 && item.buttons.length <= 0,
-            })}
-          >
-            <div className="productDesc">
-              <MultiClamp clamp={2} ellipsis={'...'}>
-                {item.description.substring(0, 59)}
-              </MultiClamp>
-            </div>
-            {item.buttons.length > 0 ? (
-              <div
-                className={
-                  item.image?.imageAspectRatio === 0
-                    ? 'rectangleImageBtn'
-                    : 'squareImageBtn'
-                }
-              >
-                {item.buttons?.map((v, i) => {
-                  return <TesterMessagesItemButton key={i} item={v} />;
-                })}
-              </div>
-            ) : (
-              <div className="rectangleImageBtn">
-                {item.buttons?.map((v, i) => {
-                  return <TesterMessagesItemButton key={i} item={v} />;
-                })}
-              </div>
-            )}
-          </div>
+          ) : null}
         </div>
       </div>
     </div>
