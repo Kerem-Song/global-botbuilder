f1c56ec79a2173563f1c1b1b27b632c208c38a34
yhsong@lunasoft.co.kr <keremcalki@gmail.com>
2023-06-27 10:33:36 +0900

    fix: gp-1427 part

--- src/components/pages/scenario/nodes/OtherFlowRedirectNode.tsx
+++ src/components/pages/scenario/nodes/OtherFlowRedirectNode.tsx
@@ -1,28 +1,20 @@
 import { useScenarioClient } from '@hooks';
 import { IScenarioModel } from '@models';
 import { IHasNode } from '@models/interfaces/IHasNode';
-import classNames from 'classnames';
 import { FC } from 'react';
-import { useParams } from 'react-router';
 
 export const OtherFlowRedirectNode: FC<IHasNode> = ({ node }) => {
-  const { botId } = useParams();
-  const { getCachedScenarioList } = useScenarioClient();
-  const data = getCachedScenarioList(botId);
-  const scenario: IScenarioModel[] | undefined = data?.filter(
+  const { getScenarioList } = useScenarioClient();
+  const { data } = getScenarioList();
+  const scenario: IScenarioModel[] = data!.filter(
     (item) => item.firstNodeId === node.nextNodeId,
   );
 
   return (
-    <>
-      {scenario?.map((item) => (
-        <div
-          className={classNames('command-node', { inactive: !item.activated })}
-          key={item.id}
-        >
-          <div>{item.alias}</div>
-        </div>
+    <div className="command-node">
+      {scenario.map((item) => (
+        <div key={item.id}>{item.alias}</div>
       ))}
-    </>
+    </div>
   );
 };
