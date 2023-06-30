bfad03beacaa0c7a4f0d489cb42b2904ba1786d1
yhsong@lunasoft.co.kr <keremcalki@gmail.com>
2023-06-30 11:20:40 +0900

    fix: gp-1481

--- src/components/pages/scenario/edit/NodeEditDrawer.tsx
+++ src/components/pages/scenario/edit/NodeEditDrawer.tsx
@@ -121,6 +121,8 @@ export const NodeEditDrawer = () => {
               required={true}
               placeholder="Input Chat Bubble name"
               {...register('title')}
+              showCount={true}
+              maxLength={100}
               disabled={selectedNode?.type === NODE_TYPES.INTENT_NODE}
               textLength={watch('title')?.length || 0}
               readOnly={isHistoryViewer || selectedNode?.type === NODE_TYPES.INTENT_NODE}
