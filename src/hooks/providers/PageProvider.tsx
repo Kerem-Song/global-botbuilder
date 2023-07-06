f8412366fc70694684027c23a5b319043c1e0b07
cain <yhhang1@lunasoft.co.kr>
2023-07-06 16:11:30 +0900

    feat: 언어처리, 페이지 이름 대문자처리

--- src/hooks/providers/PageProvider.tsx
+++ src/hooks/providers/PageProvider.tsx
@@ -23,9 +23,9 @@ export const PageContext = createContext<
 >(undefined);
 
 export const PageProvider: FC<IPageProps> = ({ pageName, isReadOnly, children }) => {
-  const { t } = useTranslation(pageName);
-  const { t: tc, i18n } = useTranslation('common');
-  const { t: ts } = useTranslation('sidebar');
+  const { t } = useTranslation(pageName.toUpperCase());
+  const { t: tc, i18n } = useTranslation('common'.toUpperCase());
+  const { t: ts } = useTranslation('sidebar'.toUpperCase());
   const [navigateUrl, setNavigateUrl] = useState<string>();
   const { error } = useSystemModal();
   const navigate = useNavigate();
