07956a02ab9510730ac8c7170c3c0f69485ac60e
N210203 <bykim@lunasoft.co.kr>
2023-07-12 10:54:02 +0900

    fix: checkbox delete, off focus

--- src/components/data-entry/Checkbox.tsx
+++ src/components/data-entry/Checkbox.tsx
@@ -12,7 +12,13 @@ export interface CheckboxProps extends IDataEntryProp, IHasClassNameNStyle {
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((args, ref) => {
   return (
     <div className="checkboxContainer" role="presentation">
-      <input {...args} className="checkbox" type="checkbox" ref={ref} />
+      <input
+        {...args}
+        className="checkbox"
+        type="checkbox"
+        ref={ref}
+        onFocus={(e) => e.target.blur()}
+      />
     </div>
   );
 });
