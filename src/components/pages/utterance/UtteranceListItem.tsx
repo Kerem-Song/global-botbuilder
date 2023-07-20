dc42a90dac8f0b87ad674fd1b39e7a61f218a9ba
N210203 <bykim@lunasoft.co.kr>
2023-07-20 14:48:59 +0900

    fix: remove MultiClamp in UtteranceListItem

--- src/components/pages/utterance/UtteranceListItem.tsx
+++ src/components/pages/utterance/UtteranceListItem.tsx
@@ -9,7 +9,6 @@ import { InfiniteData } from '@tanstack/react-query';
 import classNames from 'classnames';
 import { FC, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
-import MultiClamp from 'react-multi-clamp';
 import { useParams } from 'react-router';
 
 import { lunaToast } from '../../../../src/modules/lunaToast';
@@ -146,14 +145,14 @@ export const UtteranceListItem: FC<IUtteranceListItemProps> = ({
                       'hidden-scenarioList-utterance': !showScenarioList,
                     })}
                   >
-                    <MultiClamp clamp={1} ellipsis={'...'}>
+                    <span className="utteranceItems">
                       {searchData?.searchWord
                         ? util.replaceKeywordMark(
                             x.utteranceSummary,
                             searchData?.searchWord,
                           )
                         : x.utteranceSummary}
-                    </MultiClamp>
+                    </span>
                   </td>
                   <td className="icon">
                     <button
