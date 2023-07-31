4aad34f65de11714ccc54dba41ac6ec1f68357c5
boni <bykim@lunasoft.co.kr>
2023-07-31 17:17:07 +0900

    fix: card/cardCarousel card css, variable css

--- src/components/pages/scenario/bot-tester/TesterMessagesItem.tsx
+++ src/components/pages/scenario/bot-tester/TesterMessagesItem.tsx
@@ -41,63 +41,74 @@ export const TesterMessagesItem = ({ item, onClick }: TesterProps) => {
             )}
           </>
         );
-      case TESTER_DATA_TYPES.card:
+      case TESTER_DATA_TYPES.card: {
+        const isRectangleImage = item.image?.imageAspectRatio !== 1;
+        const hasButtons = item.buttons.length > 0;
+        const onlyCardImage =
+          item.title?.length === 0 &&
+          item.contentText?.length === 0 &&
+          item.buttons.length === 0;
         return (
           <div
             className="basicCardContainer"
             role="presentation"
             onClick={() => onClick(item.debugMeta)}
           >
-            {item.image ? (
+            {item.image && (
               <img
-                className={
-                  item.image.imageAspectRatio === 0
-                    ? 'cardImg_rectangle'
-                    : 'cardImg_square'
-                }
+                className={classNames('cardImg', {
+                  cardImgRectangle: !onlyCardImage && isRectangleImage,
+                  onlyCardImgRectangle: onlyCardImage && isRectangleImage,
+                  cardImgSquare: !onlyCardImage && !isRectangleImage,
+                  onlyCardImgSquare: onlyCardImage && !isRectangleImage,
+                })}
                 src={item.image.imageUrl}
-                alt="cardImg"
+                alt="cardCarouselImg"
                 onError={(e) => {
                   handleImgOnError(e);
                 }}
               />
-            ) : (
-              <></>
             )}
             {item.title || item.contentText ? (
-              <div className="cardText">
-                {item.title && (
+              <div
+                className={classNames('cardContents', {
+                  rectangleImageContents: isRectangleImage,
+                  squareImageContents: !isRectangleImage,
+                })}
+              >
+                <div>
                   <div className="cardTitle">
                     <MultiClamp clamp={2} ellipsis={'...'}>
-                      {item.title.substring(0, 40)}
+                      {item.title?.substring(0, 40)}
                     </MultiClamp>
                   </div>
-                )}
-                {item.contentText && (
-                  <div className="cardContentText">
-                    <MultiClamp clamp={8} ellipsis={'...'}>
-                      {item.contentText.substring(0, 230)}
+                  <div className="cardContentDesc">
+                    <MultiClamp clamp={8}>
+                      {item.contentText?.substring(0, 230)}
                     </MultiClamp>
                   </div>
-                )}
-                {item.buttons.length > 0 && (
+                </div>
+                <div className={hasButtons ? 'hasBtns' : undefined}>
                   <div
-                    className={classNames('rectangleImageBtn', {
-                      squareImageBtn: item.image?.imageAspectRatio === 1,
-                    })}
+                    className={isRectangleImage ? 'rectangleImageBtn' : 'squareImageBtn'}
                   >
-                    {item.buttons.map((v, i) => (
-                      <TesterMessagesItemButton key={i} item={v} />
-                    ))}
+                    {item.buttons.map((v, i) => {
+                      return <TesterMessagesItemButton key={i} item={v} />;
+                    })}
                   </div>
-                )}
+                </div>
               </div>
             ) : (
-              <div className="cardText">
+              <div
+                className={classNames('cardContents', {
+                  rectangleImageContents: !onlyCardImage && isRectangleImage,
+                  onlyRectangleImageContents: onlyCardImage && isRectangleImage,
+                  squareImageContents: !onlyCardImage && !isRectangleImage,
+                  onlySquareImageContents: onlyCardImage && !isRectangleImage,
+                })}
+              >
                 <div
-                  className={classNames('rectangleImageBtn', {
-                    squareImageBtn: item.image?.imageAspectRatio === 1,
-                  })}
+                  className={isRectangleImage ? 'rectangleImageBtn' : 'squareImageBtn'}
                 >
                   {item.buttons.map((v, i) => (
                     <TesterMessagesItemButton key={i} item={v} />
@@ -107,6 +118,7 @@ export const TesterMessagesItem = ({ item, onClick }: TesterProps) => {
             )}
           </div>
         );
+      }
       case TESTER_DATA_TYPES.cardCarousel:
         return (
           <div
