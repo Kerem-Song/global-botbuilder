ddc6cc31911befb2d5da1ca14b1e7905ba7b682b
yhsong@lunasoft.co.kr <keremcalki@gmail.com>
2023-08-03 17:49:23 +0900

    fix: other flow scenario alias in history viewer

--- src/components/pages/scenario/edit/OtherFlowRedirectNodeEdit.tsx
+++ src/components/pages/scenario/edit/OtherFlowRedirectNodeEdit.tsx
@@ -1,17 +1,21 @@
-import { FormItem } from '@components/data-entry';
+import { FormItem, Input } from '@components/data-entry';
 import { Space } from '@components/layout';
-import { useNodeEditSave, usePage } from '@hooks';
+import { useHistoryViewerMatch, useNodeEditSave, usePage } from '@hooks';
+import { useHistoryClient } from '@hooks/client/historyClient';
 import { useScenarioSelectClient } from '@hooks/client/scenarioSelectClient';
-import { IGNodeEditModel, IReactSelect } from '@models';
+import { IGNodeEditModel, IReactSelect, NodeOption } from '@models';
 import { IOtherFlowRedirectView } from '@models/interfaces/res/IGetFlowRes';
 import { getReactSelectStyle } from '@modules';
 import { useEffect, useState } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
+import { useParams } from 'react-router';
 import Select from 'react-select';
 
 export const OtherFlowRedirectNodeEdit = () => {
   useNodeEditSave();
   const { t } = usePage();
+  const { botId, historyId } = useParams();
+  const isHistoryViewer = useHistoryViewerMatch();
   const [scenarioList, setScenarioList] = useState<IReactSelect[]>([
     { value: '', label: t(`SET_OPTION_NULL`) },
   ]);
@@ -29,6 +33,28 @@ export const OtherFlowRedirectNodeEdit = () => {
   });
 
   const values = getValues();
+
+  const handleNodeHistoryViewer = () => {
+    if (isHistoryViewer) {
+      const { getFlowSnapShot } = useHistoryClient();
+      const { data: historyData } = getFlowSnapShot({
+        botId: botId!,
+        historyId: historyId!,
+      });
+      const scenarioInHistoryViewer = historyData?.result.nodes.filter(
+        (item) =>
+          item.option === NodeOption.Wormhole && item.nextNodeId === values.nextNodeId,
+      );
+
+      return (
+        scenarioInHistoryViewer &&
+        scenarioInHistoryViewer.length &&
+        scenarioInHistoryViewer.map((itemInHistory) => (
+          <Input key={itemInHistory.id} value={itemInHistory.view?.otherFlowAlias} />
+        ))
+      );
+    }
+  };
 
   console.log('values in other flow redirect node edit:', values);
   console.log('data in ofr', data);
@@ -55,18 +81,24 @@ export const OtherFlowRedirectNodeEdit = () => {
             <span className="subLabel">{t(`OTHER_FLOW_REDIRECT_NODE_SET`)} </span>
             <span className="required">*</span>
           </div>
-          <FormItem error={errors.nextNodeId}>
-            <Select
-              className="react-selector"
-              {...field}
-              options={scenarioList}
-              placeholder={t(`SET_OPTION_NULL`)}
-              styles={reactSelectStyle}
-              defaultValue={scenarioList.find((item) => item.value === values.nextNodeId)}
-              value={scenarioList.find((item) => item.value === field.value)}
-              onChange={(options: any) => field.onChange(options?.value)}
-            />
-          </FormItem>
+          {isHistoryViewer ? (
+            handleNodeHistoryViewer()
+          ) : (
+            <FormItem error={errors.nextNodeId}>
+              <Select
+                className="react-selector"
+                {...field}
+                options={scenarioList}
+                placeholder={t(`SET_OPTION_NULL`)}
+                styles={reactSelectStyle}
+                defaultValue={scenarioList.find(
+                  (item) => item.value === values.nextNodeId,
+                )}
+                value={scenarioList.find((item) => item.value === field.value)}
+                onChange={(options: any) => field.onChange(options?.value)}
+              />
+            </FormItem>
+          )}
         </Space>
       </div>
     </div>
