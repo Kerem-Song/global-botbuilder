99cab8f9ba986bd4d2308c7307ee3f7a5b7015d6
boni <bykim@lunasoft.co.kr>
2023-08-08 12:17:53 +0900

    fix: GP-1658

--- src/components/pages/dashboard/NewBotCard.tsx
+++ src/components/pages/dashboard/NewBotCard.tsx
@@ -7,11 +7,16 @@ import { FC } from 'react';
 
 export const NewBotCard: FC<{ onClick: () => void }> = ({ onClick }) => {
   const { t, tc } = usePage();
+  const role = useRootState((state) => state.userInfoReducer.role);
   const staffType = useRootState((state) => state.userInfoReducer.staffType);
   const { error } = useSystemModal();
 
+  const isAdministrator = staffType === StaffType.Administrator && role === 0;
+  const isManager = staffType === StaffType.Manager;
+  const isRoleValid = role !== undefined && role <= 114;
+
   const handleOnClickAuth = async () => {
-    if (staffType === StaffType.Manager) {
+    if (isManager || (!isAdministrator && isRoleValid)) {
       await error({
         title: tc(`NEW_BOT_CARD_AUTH_ERROR_TITLE`),
         description: (
@@ -31,10 +36,15 @@ export const NewBotCard: FC<{ onClick: () => void }> = ({ onClick }) => {
     <Card
       bordered={false}
       onClick={handleOnClickAuth}
-      className={classNames('new-chatbot', { disabled: staffType === StaffType.Manager })}
+      className={classNames('new-chatbot', {
+        disabled: isManager || (!isAdministrator && isRoleValid),
+      })}
     >
       <div className="title">
-        <img src={staffType === StaffType.Manager ? icAddDisable : icAdd} alt="add" />
+        <img
+          src={isManager || (!isAdministrator && isRoleValid) ? icAddDisable : icAdd}
+          alt="add"
+        />
         <Title level={3}>{t('NEW_BOT_TITLE')}</Title>
       </div>
       <div className="title-hover">
@@ -42,7 +52,9 @@ export const NewBotCard: FC<{ onClick: () => void }> = ({ onClick }) => {
           <path
             d="M 7 0 L 7 14 M 0 7 L 14 7"
             strokeWidth={2}
-            stroke={staffType === StaffType.Manager ? '#929292' : '#6993FF'}
+            stroke={
+              isManager || (!isAdministrator && isRoleValid) ? '#929292' : '#6993FF'
+            }
             fill="none"
           />
         </svg>
