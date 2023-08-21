138df1bfc9e9d67697df7ae41b0c45fce513ae1c
cain <yhhang1@lunasoft.co.kr>
2023-08-21 15:36:41 +0900

    fix: https://lunasoft.atlassian.net/browse/GP-1784

--- src/hooks/useSelectedScenarioChange.ts
+++ src/hooks/useSelectedScenarioChange.ts
@@ -1,37 +1,13 @@
 import { IScenarioModel } from '@models';
-import { setSelectedScenario } from '@store/botbuilderSlice';
-import { createElement } from 'react';
-import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
 
 import usePage from './usePage';
-import { useRootState } from './useRootState';
-import { useSystemModal } from './useSystemModal';
 
 export const useSelectedScenarioChange = () => {
-  const { tc, navigate } = usePage();
+  const { navigate } = usePage();
   const { botId } = useParams();
-  const dispatch = useDispatch();
-  const { confirm } = useSystemModal();
-  const changed = useRootState((state) => state.makingNodeSliceReducer.present.changed);
 
   const handleChangeSelectedScenario = async (item?: IScenarioModel) => {
-    if (changed) {
-      const message = createElement(
-        'span',
-        { style: { whiteSpace: 'pre-line' } },
-        tc('SAVE_CONFIRM_MESSAGE'),
-      );
-      const result = await confirm({
-        title: tc('SAVE_CONFIRM_TITLE'),
-        description: message,
-      });
-
-      if (!result) {
-        return false;
-      }
-    }
-    // dispatch(setSelectedScenario(item));
     if (item?.isStartFlow) {
       navigate(`/${botId}/scenario/start`);
     } else if (item?.isFallbackFlow) {
