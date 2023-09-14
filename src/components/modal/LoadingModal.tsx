dbc4e1ee2598294a62a699b4fcd91ed6e5df7366
yhsong@lunasoft.co.kr <keremcalki@gmail.com>
2023-09-14 16:20:57 +0900

    fix: saving modal overlay background

--- src/components/modal/LoadingModal.tsx
+++ src/components/modal/LoadingModal.tsx
@@ -16,6 +16,7 @@ export const LoadingModal: FC<ILavingModalProps> = ({ isOpen, title, desc }) => 
       style={{ overlay: { display: 'flex' } }}
       className="loadingModal"
       isOpen={isOpen}
+      overlayClassName="luna-system-modal-overlay"
     >
       <div className="contents">
         <ReactLoading type="spin" color="#4478FF" height={50} width={50} />
